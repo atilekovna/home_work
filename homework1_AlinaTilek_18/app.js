78ae677c2a891d0fbca6e51a6a154f71c7b81f94
atilekovna <113083993+atilekovna@users.noreply.github.com>
2022-09-19 01:10:30 +0600

    Upload home work #1

---
+++ homework1_AlinaTilek_18/app.js
@@ -0,0 +1,21 @@
+var customer = {
+    AccountBalance: "namber",
+    AccountNumber: "string",
+    AccountCurrency: "string",
+    AccountHolderName: {
+        Bank: "string",
+        Address: "string",
+    }
+}
+console.log(customer);
+
+
+// Условия..
+
+var customerType = 1;
+
+if(customerType ===1) {
+    console.log("физ.лицо"); 
+} else {
+    console.log("юр.лицо"); 
+}
