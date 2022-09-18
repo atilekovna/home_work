eb239896f8887d3fe3ad6cfb03b18449bca3999d
atilekovna <113083993+atilekovna@users.noreply.github.com>
2022-09-19 01:13:55 +0600

    Upload home work #4

---
+++ homework4_Alina Tilek_18/js/app.js
@@ -0,0 +1,6 @@
+var heght = 7;
+var row = "";
+for (var i = 0; i < heght; i++) {
+    row += "*";
+    console.log(row);
+}
---
+++ homework4_Alina Tilek_18/js/script.js
@@ -0,0 +1,12 @@
+var heght = 6;
+var result;
+for (var i = 0; i < heght; i++) {
+
+  result = i%2;
+
+  if (result === 0) {
+    console.log(' # # # # #');
+  } else {
+    console.log('# # # # #');
+  }
+}
